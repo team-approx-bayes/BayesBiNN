4fd3214351d8e13eb0b0f28f9e90f938b064160f
mengxiangming <mengxm11@gmail.com>
2020-05-03 15:03:35 +0900

    Update main_cifar10.py

--- main_cifar10.py
+++ main_cifar10.py
@@ -58,8 +58,6 @@ def main():
     parser.add_argument('--decay-steps', type=int, default=1, metavar='N',
                         help='LR rate decay steps (default: 1)')   
 
-    parser.add_argument('--tp-decay', type=float, default= 1, metavar='LR-decay',
-                        help='temperature decay factor for BayesBiNN optimizer each epoch (default: 1.0)')
     parser.add_argument('--momentum', type=float, default=0.0, metavar='M',
                         help='BayesBiNN momentum (default: 0.9)')
     parser.add_argument('--data-augmentation', action='store_true', default=True, help='Enable data augmentation')
@@ -87,7 +85,6 @@ def main():
 
     parser.add_argument('--lamda-std', type=float, default= 0, metavar='lamda-init',
                         help='linitial std value of the natural parameter lamda(default: 0)')
-
 
     parser.add_argument('--temperature', type=float, default= 1e-10, metavar='temperature',
                         help='temperature for BayesBiNN')
